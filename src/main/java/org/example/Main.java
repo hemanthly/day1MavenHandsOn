dc5e6c86ef59386fff5cc15166d4468b336f634c
Hemanth <hemanthlishetti999@gmail.com>
2024-02-17 19:23:23 +0530

    jenkins integration check commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("for the second commit!");
+        System.out.println("Jenkins integration check!!");
     }
 }
